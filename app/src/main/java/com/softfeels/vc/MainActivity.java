933493caea969a08b4e5ab6b436269f9a6f281e1
ravindra.bagale <ravindra.bagale@hotmail.com>
2020-05-20 01:43:41 +0530

    start test

---
+++ app/src/main/java/com/softfeels/vc/MainActivity.java
@@ -0,0 +1,17 @@
+package com.softfeels.vc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String S= getApplicationContext().getPackageName();
+        Log.e("App=",S);
+    }
+}
